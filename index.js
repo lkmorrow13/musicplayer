dc659c7f02668d2a3f3327adfcaa0208da795a86
Laven Morrow <lmorrow13@live.com>
2018-01-24 15:05:36 -0700

    finished rough draft

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
-class JukeBox {
-	
+class Jukebox {
+	constructor(audio) {
+		this.song = audio;
+		this.songs = ['01overture.m4a', '04outfoxthefox.m4a', '07livetolove.m4a']
+		this.currentSongIndex = 0;
+	}
+
+	playAudio() {
+		this.song.play();
+	}
+
+	pauseAudio() {
+		this.song.pause();
+	}
+
+	stopAudio() {
+		this.song.pause();
+		this.song.currentTime = 0;
+	}
+
+	nextAudio() {
+		this.currentSongIndex++;
+		document.getElementById('myAudio').setAttribute('src', 'songs/' + this.songs[this.currentSongIndex]);
+		
+	}
 }
+
+var audio = document.getElementById('myAudio');
+var jukebox = new Jukebox(audio);
+
+document.getElementById('playbutton').addEventListener('click', function() {
+	jukebox.playAudio();
+})
+
+document.getElementById('pausebutton').addEventListener('click', function() {
+	jukebox.pauseAudio();
+})
+
+document.getElementById('stopbutton').addEventListener('click', function() {
+	jukebox.stopAudio();
+})
+
+document.getElementById('nextbutton').addEventListener('click', function() {
+	jukebox.nextAudio();
+})
